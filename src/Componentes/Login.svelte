9a4575669e3a543452ce4dfe912773f404a95761
hectorArredondo34 <113058902+hectorArredondo34@users.noreply.github.com>
2024-11-27 15:48:45 -0600

    update

--- src/Componentes/Login.svelte
+++ src/Componentes/Login.svelte
@@ -4,8 +4,51 @@
 
 <main>
 
+    <div class="row">
+        <div class="col">
+            <img src="../../img/login.JPG" alt="">
+        </div>
+        <div class="col credencial">
+            <div class="row">
+                <div class="input-group flex-nowrap correo">
+                    <span class="input-group-text email" id="addon-wrapping"> Correo: </span>
+                    <input type="e-mail" class="form-control" placeholder="Correo ENCRyM o INAH" aria-label="Correo ENCRyM o INAH" aria-describedby="addon-wrapping">
+                </div>
+                <div class="input-group flex-nowrap pass">
+                    <span class="input-group-text" id="addon-wrapping"> Contraseña: </span>
+                    <span class="input-group-text" id="addon-wrapping"> <i class="bi bi-eye-slash-fill"></i> </span>
+                    <input type="password" class="form-control" placeholder="" aria-label="" aria-describedby="addon-wrapping">
+                </div>
+            </div>
+        </div>
+    </div>
+
 </main>
 
 <style>
+
+    img {
+        width: 50%;
+        height: 100%;
+        margin-top: 45px;
+        margin-left: 180px;
+    }
+
+    .credencial {
+        margin-top: 80px;
+        margin-right: 140px;
+    }
+
+    .email {
+        width: 155px;
+    }
+
+    .pass {
+        margin-top: 40px;
+    }
+
+    .correo, .pass {
+        width: 580px;
+    }
     
 </style>
